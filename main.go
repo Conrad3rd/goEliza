1a0f85473f4b4c1a0c80bb254a4ba7493e80bfe5
Conrad Esche <conrad.3rd@gmail.com>
2023-03-02 10:56:55 +0100

    read userinput

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 
 	"github.com/conrad3rd/goMods/mods/doctor"
 	"github.com/inancgumus/screen"
@@ -10,10 +12,16 @@ import (
 func main() {
 	// Clear all the characters on the screen
 	screen.Clear()
-
 	// Moves the cursor to the top-left position of the screen
 	screen.MoveTopLeft()
 
+	reader := bufio.NewReader(os.Stdin)
+
 	whatToSay := doctor.Intro()
+
 	fmt.Println(whatToSay)
+
+	userInput, _ := reader.ReadString('\n')
+
+	fmt.Println(userInput)
 }
